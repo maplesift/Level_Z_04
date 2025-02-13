07422e04cafc9a958ff02a943240f08110853d36
maplesift <a23050716@gmail.com>
2025-02-13 11:54:44 +0800

    z04 完成頁尾版權更新功能

--- api/db.php
+++ api/db.php
@@ -112,3 +112,4 @@ function dd($array){
 
 $Mem=new DB("members");
 $Admin=new DB("admins");
+$Bot=new DB("bottom");
--- api/save_admin.php
+++ api/save_admin.php
@@ -1,7 +1,13 @@
 <?php include_once "db.php";
 
+// 增加判斷式:如果管理者權限為空-增加空陣列 (考試可不加)
+if(!empty($_POST['pr'])){
 
-$_POST['pr']=serialize($_POST['pr']);
+    $_POST['pr']=serialize($_POST['pr']);
+}else{
+    $_POST['pr']=serialize([]);
+}
+
 $Admin->save($_POST);
 
 to("../admin.php?do=admin");
